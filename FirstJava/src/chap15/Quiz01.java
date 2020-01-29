924e3d73aed6b22c2dbe16c81baca39bd6e0fca3
Yang Hanah <toaur6802@bluedigm.com>
2020-01-29 10:46:20 +0900

    chap15 quiz

---
+++ FirstJava/src/chap15/Quiz01.java
@@ -0,0 +1,38 @@
+package chap15;
+
+class Box{
+	public void wrap() {
+		System.out.println("Simple Wrapping");
+	}
+}
+
+class PaperBox extends Box{
+	public void wrap() {
+		System.out.println("Paper Wrapping");
+	}
+}
+
+class GoldPaperBox extends PaperBox{
+	public void wrap() {
+		System.out.println("Gold Wrapping");
+	}
+}
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+
+		Box box1 = new Box();
+		PaperBox box2 = new PaperBox();
+		GoldPaperBox box3 = new GoldPaperBox();
+		
+		wrapBox(box1); 
+		wrapBox(box2); 
+		wrapBox(box3);
+	}
+	
+	public static void wrapBox(Box box) {
+		box.wrap();
+	}
+}
+
